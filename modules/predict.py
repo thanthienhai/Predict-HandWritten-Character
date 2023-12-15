03546e48f57e8a841d767b2667cc1747101807b0
Than Thien <50171638+thanthienhai@users.noreply.github.com>
2023-12-15 12:04:47 +0700

    Model trained with 10 epochs

---
+++ modules/predict.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+
+model = load_model('modela_z.h5')
+
+def resize_image(img, size=(28,28), border = 0.7857):
+
+    h, w = img.shape[:2]
+    c = img.shape[2] if len(img.shape)>2 else 1
+
+    if h == w:
+        return cv2.resize(img, size, cv2.INTER_AREA)
+
+    dif = h if h > w else w
+    dif = int(dif // border)
+
+    mode = cv2.INTER_AREA if dif > (size[0]+size[1])//2 else cv2.INTER_CUBIC
+
+    x_pos = (dif - w)//2
+    y_pos = (dif - h)//2
+
+    if len(img.shape) == 2:
+        mask = np.multiply(np.ones((dif, dif), dtype=img.dtype), 255.0)
+        mask[y_pos:y_pos+h, x_pos:x_pos+w] = img[:h, :w]
+    else:
+        mask = np.multiply(np.ones((dif, dif, c), dtype=img.dtype), 255.0)
+        mask[y_pos:y_pos+h, x_pos:x_pos+w, :] = img[:h, :w, :]
+
+    return cv2.resize(mask, size, mode)
+
+
+def predict_digit(path_img):
+    # Change PIL to CV2
+    # pil_image = img.convert('RGB')
+    # open_cv_image = numpy.array(pil_image)
+    # open_cv_image = open_cv_image[:, :, ::-1].copy()
+    # img = open_cv_image
+    img = cv2.imread(path_img)
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # get coordinates of bounding digit box
+    tmp, imgBi = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV)
+    contours, tmp = cv2.findContours(imgBi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    bounding_boxes = [cv2.boundingRect(cnt) for cnt in contours]
+
+    list_num = list()
+    for i in bounding_boxes:
+        x, y, w, h = i
+
+        cropImg = img.copy()
+        # Crop image with bounding digit box
+        cropImg = cropImg[y:y+h, x:x+w]
+
+        # Resize image (28,28)
+        # cropImg = cv2.resize(cropImg, (28, 28), interpolation = cv2.INTER_LINEAR)
+        cropImg = resize_image(cropImg, (28, 28))
+
+        new_img = np.array(cropImg)
+        new_img = new_img.reshape(1, 28, 28, 1)
+        new_img = new_img / 255.0
+
+        # convert image to black background
+        one = np.ones((1, 28, 28, 1), dtype=float)
+        new_img = one - new_img
+        # plt.imshow(img.reshape(28,28), cmap='gray')
+        # plt.show()
+
+        # predicting the class
+        res = model.predict([new_img])[0]
+        list_num.append([np.argmax(res), max(res)])
+    return list_num[::-1]
+
---
+++ streamlit.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+from PIL import Image
+from modules.predict import predict_digit
+
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+choice = "Image"
+
+LABELS = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
+if choice == "Image":
+
+    st.subheader("Dự đoán chữ số viết tay")
+    st.caption('Trang web này giúp bạn dự đoán chữ số viết tay với độ chính xác tương đối :)))')
+    st.caption('Bạn hãy vẽ một bức ảnh có chứa một số bằng paint hoặc bất kì app nào khác, đừng vẽ thêm những thứ khác')
+    st.caption('Sau đó lưu lại và upload vào đây nhé!!!')
+    image_file = st.file_uploader("Upload Images",
+                                  type=["png", "jpg", "jpeg"])
+
+    if image_file is not None:
+        # TO See details
+        file_details = {"filename": image_file.name, "filetype": image_file.type,
+                        "filesize": image_file.size}
+        st.write(file_details)
+        st.image(load_image(image_file), width=250)
+
+        # Saving upload
+        with open(os.path.join("fileDir", image_file.name), "wb") as f:
+            f.write((image_file).getbuffer())
+
+        st.success("File Saved")
+        num = predict_digit(os.path.join("fileDir", image_file.name))
+
+        st.write("Các số bạn đã vẽ là: ", ", ".join([LABELS[i[0]] for i in num]))
+        st.write("Độ chính xác tương ứng là: ", ", ".join([ str(round(i[1]*100.0, 2)) for i in num]))
+
